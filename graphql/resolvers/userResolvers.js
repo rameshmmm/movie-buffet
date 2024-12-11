a74337d2cbfb6f1fbad8ca7876744aea1be31718
MuraliVenkat <murali.venkatesh@cdw.com>
2024-12-11 17:51:23 +0530

    wip

--- graphql/resolvers/userResolvers.js
+++ graphql/resolvers/userResolvers.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { Users } = require('../../models/stepDbSchema');
 const { ERROR_MESSAGES } = require('../../constants/constants');
 const { encryptPassword, decryptPassword } = require('../../utils/dataEncryptionUtils');
+const { v4: uuidv4 } = require('uuid'); // Import UUID for userId generation
 
 const userResolvers = {
   Mutation: {
@@ -40,34 +41,74 @@ const userResolvers = {
       }
     },
 
+    // register: async (_, { input }) => {
+    //   try {
+    //     const { userId, username, password } = input;
+
+    //     if (!userId || !username || !password) {
+    //       throw new GraphQLError(ERROR_MESSAGES.MISSING_PAYLOAD.MESSAGE);
+    //     }
+
+    //     const existingUser = await Users.findOne({ userId });
+    //     if (existingUser) {
+    //       throw new GraphQLError(ERROR_MESSAGES.USER_EXIST.MESSAGE);
+    //     }
+
+    //     const hashedPassword = encryptPassword(password);
+    //     const newUser = new Users({
+    //       userId,
+    //       username,
+    //       password: hashedPassword,
+    //     });
+
+    //     await newUser.save();
+
+    //     return {
+    //       userId: newUser.userId,
+    //       username: newUser.username,
+    //     };
+    //   } catch (error) {
+    //     throw new GraphQLError(error.message);
+    //   }
+    // },
+
     register: async (_, { input }) => {
       try {
-        const { userId, username, password } = input;
+        const { username, password } = input;
 
-        if (!userId || !username || !password) {
+        // Validate input
+        if (!username || !password) {
           throw new GraphQLError(ERROR_MESSAGES.MISSING_PAYLOAD.MESSAGE);
         }
 
-        const existingUser = await Users.findOne({ userId });
+        // Check if username already exists
+        const existingUser = await Users.findOne({ username });
         if (existingUser) {
           throw new GraphQLError(ERROR_MESSAGES.USER_EXIST.MESSAGE);
         }
 
+        // Generate a unique userId
+        const userId = uuidv4();
+
+        // Encrypt the password
         const hashedPassword = encryptPassword(password);
+
+        // Create and save the new user
         const newUser = new Users({
           userId,
           username,
           password: hashedPassword,
         });
-
         await newUser.save();
 
+        console.log(newUser)
+        // Return user details
         return {
           userId: newUser.userId,
           username: newUser.username,
         };
       } catch (error) {
-        throw new GraphQLError(error.message);
+        throw new GraphQLError(error.message || 'Failed to register user');
       }
     },
   },
